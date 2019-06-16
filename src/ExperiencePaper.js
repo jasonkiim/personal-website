19d3c55a89b9a0a7843b82bbf0618b3c34ec26e7
jasonkiim <dmdwn2696@naver.com>
2019-06-16 14:26:11 -0400

    Add classnames package and implement flex styles

--- src/ExperiencePaper.js
+++ src/ExperiencePaper.js
@@ -2,6 +2,8 @@ const React = require('react');
 const createReactClass = require('create-react-class');
 const PropTypes = require('prop-types');
 
+const flexStyles = require('./styles/FlexStyles.css');
+
 const ExperiencePaper = createReactClass({
   displayName: 'ExperiencePaper',
   propTypes: {
