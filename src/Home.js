6a29ac537e725be2f5efab6458ad2e8b46f70e98
jasonkiim <dmdwn2696@naver.com>
2019-06-15 23:07:28 -0400

    Sync LoadingPage styles to Home styles

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
 const React = require('react');
 const createReactClass = require('create-react-class');
-
 const { FontAwesomeIcon } = require('@fortawesome/react-fontawesome');
-const { faCoffee } = require('@fortawesome/free-solid-svg-icons');
+const { faInbox } = require('@fortawesome/free-solid-svg-icons');
+const {
+  faLinkedin,
+  faGithub,
+  faInstagram
+} = require('@fortawesome/free-brands-svg-icons');
 
 const Home = createReactClass({
   displayName: 'Home',
@@ -53,28 +57,51 @@ const Home = createReactClass({
               fadingLogo1 ? 'home-logos-fading' : 'home-logos-faded'
             }`}
           >
-            <FontAwesomeIcon icon={faCoffee} />
+            <a
+              href="https://www.linkedin.com/in/jasonkiim/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
           </span>
           <span
             className={`${
               fadingLogo2 ? 'home-logos-fading' : 'home-logos-faded'
             }`}
           >
-            <FontAwesomeIcon icon={faCoffee} />
+            <a href="mailto:e53kim@edu.uwaterloo.ca" className="icon">
+              <FontAwesomeIcon icon={faInbox} size="2x" />
+            </a>
           </span>
           <span
             className={`${
               fadingLogo3 ? 'home-logos-fading' : 'home-logos-faded'
             }`}
           >
-            <FontAwesomeIcon icon={faCoffee} />
+            <a
+              href="https://github.com/jasonkiim"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
           </span>
           <span
             className={`${
               fadingLogo4 ? 'home-logos-fading' : 'home-logos-faded'
             }`}
           >
-            <FontAwesomeIcon icon={faCoffee} />
+            <a
+              href="https://www.instagram.com/pprzzi_/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
           </span>
         </div>
       </div>
